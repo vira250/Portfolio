9f7c71c3083431adf8b17d1cf69fef699a86c372
vira250 <jviraj666@gmail.com>
2025-02-01 20:19:09 +0530

    add typeing effect

--- js/main.js
+++ js/main.js
@@ -25,3 +25,14 @@ function headerShadow(){
         navHeader.style.lineHeight = "90px";
     }
 }
+
+
+// typing effect
+
+var typingEffect = new Typed(".typedText",{
+    strings : ["Web Developer","Application Developer"],
+    loop:true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
